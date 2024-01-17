7ee56b6e471a63a1b5e38e2d7a06d514d30ff142
Pamela <pamela.woods@hafogvatn.is>
2024-01-17 22:13:14 +0000

    *finished* SPM link

---
+++ README.R
@@ -0,0 +1,17 @@
+##Uploading material the first time
+
+#navigate: cd /net/hafri/export/home/hafri/pamela/public_html
+#copy: cp -a ~/Documents/Hafro/UNU/2024/PopModels/_site .
+#move: mv _site population_dynamics
+#permissions: chmod 777 population_dynamics
+#check:ls -l
+
+## RE-uploading material
+
+#Render here, check index.html
+#remove old: rm -r population_dynamics/
+#copy: cp -a ~/Documents/Hafro/UNU/2024/PopModels/_site .
+#move: mv _site population_dynamics
+#permissions: chmod 777 population_dynamics
+#check: ls -l
+# go to website heima..
